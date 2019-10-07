e0aabde4da3e5e0d445be81a12d8c1b8aa880347
Jan Müller <janmueller3698@gmail.com>
2019-10-07 12:20:13 +0200

    Readded correct appdata usage

--- src/main/kotlin/eu/yeger/dplc/PersistencyController.kt
+++ src/main/kotlin/eu/yeger/dplc/PersistencyController.kt
@@ -22,7 +22,8 @@ object PersistencyController {
 
     private fun loadDefaults() = String(this.javaClass.getResourceAsStream("/defaults.json").readAllBytes())
 
-    private fun getAppDirectory() = when(System.getProperty("os.name")) {
-        else -> File(PersistencyController::class.java.protectionDomain.codeSource.location.toURI()).parentFile
+    private fun getAppDirectory() = when {
+        System.getProperty("os.name").contains("Win") -> System.getenv("AppData")
+        else -> File(PersistencyController::class.java.protectionDomain.codeSource.location.toURI()).parentFile.absolutePath
     }
 }
